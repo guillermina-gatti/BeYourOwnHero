41e9372af91bfafd3d1998543a0744baec9ddc1b
Guillermina Gatti <yimina777@gmail.com>
2026-09-10 16:45:49 -0300

    feat: add api welcome route

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: ['/'],
+  });
 
   app.use(helmet());
 
