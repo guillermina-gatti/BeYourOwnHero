46ef65c908ca03f6eba7ad3f30e93193b92b2044
Guillermina Gatti <yimina777@gmail.com>
2026-09-10 13:40:40 -0300

    feat: register habits module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validate } from './config/env.validation';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { HabitsModule } from './habits/habits.module';
 
 @Module({
   imports: [
@@ -16,6 +18,7 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     UsersModule,
     AuthModule,
+    HabitsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
